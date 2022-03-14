35178adc05851103ca1422ebdaa4b866524d609a
Isaiah-Mck <Mr.isaiahmck@gmail.com>
2022-03-14 12:33:58 -0700

    Added Search by Number & Mana

--- DICE_MENU 1.0.0/Assets/Scripts/UIManager.cs
+++ DICE_MENU 1.0.0/Assets/Scripts/UIManager.cs
@@ -7,6 +7,9 @@ public class UIManager : MonoBehaviour{
 
     public CardManager cardManager;
     public GameObject[] cardSlots;
+
+	// For Card-Class colors 
+	// public Color humanColor, orcColor, dragonColor, beastColor, otherColor;
     
     public int page;
     public Text pageText;
@@ -15,8 +18,53 @@ public class UIManager : MonoBehaviour{
 		UpdatePage();
 
 		TurnPage();
+	}
 
-		if(Input.GetKeyDown(KeyCode.D)){	// Next Page
+	private void UpdatePage(){
+		// pageText.text = (page + 1).ToString();
+        pageText.text = (page + 1) + "/" + (Mathf.Ceil(cardSlots.Length / 8) +1).ToString();
+	}
+
+    private void CallWhenTurnPage(){
+        for (int i = 0; i<cardManager.cards.Count; i++){
+            if (i>=page *8 && i<(page + 1) *8){
+                DisplaySingleCard(i);
+            } else { 
+                cardSlots[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+	public void SearchByMana(int _mana) {
+			for (int i = 0; i < cardManager.cards.Count; i++){
+				if (_mana < 8){
+				if(cardManager.cards[i].manaCost == _mana){
+						DisplaySingleCard(i);
+				} else {
+						cardSlots[i].gameObject.SetActive(false);
+					} 
+				} else {
+				if(cardManager.cards[i].manaCost >= _mana){
+					DisplaySingleCard(i);
+				} else {
+					cardSlots[i].gameObject.SetActive(false);
+				}
+			}
+		}
+	}
+
+	public void SearchByClass(string _cardClass){
+		for (int i = 0; i < cardManager.cards.Count; i++){
+			if(cardManager.cards[i].cardClass.ToString() == _cardClass){
+				DisplaySingleCard(i);
+			} else {
+				cardSlots[i].gameObject.SetActive(false);
+			}
+		}
+	}
+	
+	private void TurnPage() {
+			if(Input.GetKeyDown(KeyCode.D)){	// Next Page
 			// iIf the page is greater than the cards total number divided by 8 --> page turn back to 0
 			if(page >= Mathf.Floor((cardManager.cards.Count -1) /8 )){
 				page = 0;
@@ -39,55 +87,30 @@ public class UIManager : MonoBehaviour{
 		}
 	}
 
-	private void UpdatePage(){
-		// pageText.text = (page + 1).ToString();
-        pageText.text = (page + 1) + "/" + (Mathf.Ceil(cardSlots.Length / 8) +1).ToString();
+	// CODE REFACTORING
+	private void DisplaySingleCard(int i) {
+		cardSlots[i].gameObject.SetActive(true);
 	}
 
-    private void CallWhenTurnPage(){
-        
-        for (int i = 0; i<cardManager.cards.Count; i++){
-            if (i>=page *8 && i<(page + 1) *8){
-                cardSlots[i].gameObject.SetActive(true);
-            } else { 
-                cardSlots[i].gameObject.SetActive(false);
-            }
-        }
-    }
+	#region Code Refactoring
+	// public void SearchByMana(int _mana) {
+	// 	for (int i = 0; i < cardManager.cards.Count; i++){
+	// 		if(cardManager.cards[i].manaCost == _mana){
+	// 			cardSlots[i].gameObject.SetActive(true);
+	// 		} else {
+	// 			cardSlots[i].gameObject.SetActive(false);
+	// 		}
+	// 	}
+	// }
 
-	public void SearchByMana(int _mana) {
-		for (int i = 0; i < cardManager.cards.Count; i++){
-			if(cardManager.cards[i].manaCost == _mana){
-				cardSlots[i].gameObject.SetActive(true);
-			} else {
-				cardSlots[i].gameObject.SetActive(false);
-			}
-		}
-	}
-
-	public void SearchByMoreMana(int _mana) {
-		for (int i = 0; i < cardManager.cards.Count; i++){
-			if(cardManager.cards[i].manaCost >= _mana){
-				cardSlots[i].gameObject.SetActive(true);
-			} else {
-				cardSlots[i].gameObject.SetActive(false);
-			}
-		}
-	}
-
-
-
-	public void SearchByClass(string _cardClass){
-		for (int i = 0; i < cardManager.cards.Count; i++){
-			if(cardManager.cards[i].cardClass.ToString() == _cardClass){
-				cardSlots[i].gameObject.SetActive(true);
-			} else {
-				cardSlots[i].gameObject.SetActive(false);
-			}
-		}
-	}
-	
-	private void TurnPage() {
-		
-	}
+	// public void SearchByMoreMana(int _mana) {
+	// 	for (int i = 0; i < cardManager.cards.Count; i++){
+	// 		if(cardManager.cards[i].manaCost >= _mana){
+	// 			cardSlots[i].gameObject.SetActive(true);
+	// 		} else {
+	// 			cardSlots[i].gameObject.SetActive(false);
+	// 		}
+	// 	}
+	// }
+	#endregion
 }
